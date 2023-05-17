c4786d7ebd02879b749ff7b051443d80229ae339
zero <zero@localhost.localdomain>
2023-05-17 12:11:33 -0500

    Shodan

---
+++ ch3/shodan/shodan/api.go
@@ -0,0 +1,21 @@
+package shodan
+
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+)
+
+func (s *Client) APIInfo() (*APIInfo, error) {
+	res, err := http.Get(fmt.Sprintf("%s/api-info?key=%s", BaseURL, s.apiKey))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var ret APIInfo
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
